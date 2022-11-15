e5172712efab050bf8e68cc747303afdac1fdfd9
Thomas Holdom <85081553+TepidJesus@users.noreply.github.com>
2022-11-16 11:25:46 +1300

    Overridden Equality Check

--- src/CardDeck.java
+++ src/CardDeck.java
@@ -27,6 +27,7 @@ public class CardDeck {
     ArrayList<Card> deck = new ArrayList<Card>();
     SecureRandom randomGen = new SecureRandom();
     CardDeck () {
+
          for (String suit : suits) {
              for (Integer value : values) {
                  deck.add(new Card(value, suit));
@@ -42,11 +43,15 @@ public class CardDeck {
         return deck.remove(rand);
     }
 
+    public void removeCard(Card card) {
+        deck.remove(card);
+    }
+
     private static class Card {
         int rank;
         String suit;
 
-        Card (int value, String suit) {
+        Card(int value, String suit) {
             this.rank = value;
             this.suit = suit;
 
@@ -62,6 +67,25 @@ public class CardDeck {
 
         public String getSuit() {
             return this.suit;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if (obj == null) {
+                return false;
+            }
+            if (!Card.class.isAssignableFrom(obj.getClass())) {
+                return false;
+            }
+
+            final Card other = (Card) obj;
+            if (this.rank != other.rank) {
+                return false;
+            }
+            if (this.suit.equals(other.suit)) {
+                return false;
+            }
+            return true;
         }
     }
 
