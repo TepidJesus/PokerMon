b19e75852632a37e57c5a74b847ba999488bc0ba
Thomas Holdom <85081553+TepidJesus@users.noreply.github.com>
2022-12-01 17:52:39 +1300

    Made Main more OOP

--- src/MainLoop.java
+++ src/MainLoop.java
@@ -1,37 +1,40 @@
 public class MainLoop {
 
     public static void main(String[] args) {
-
-        // create a new deck
-        // run a simulation
-        // print the results
-
-
-        CardDeck deck = new CardDeck();
         Card[] heroCards = new Card[2];
         Card[][] villainCards = new Card[2][2];
 
         heroCards[0] = new Card(Card.ACE, Card.CLUBS);
         heroCards[1] = new Card(Card.ACE, Card.DIAMONDS);
-        System.out.println("Hero Cards: " + heroCards[0].toString() + " & " + heroCards[1].toString());
 
         villainCards[0][0] = new Card(Card.QUEEN, Card.HEARTS);
         villainCards[0][1] = new Card(Card.DEUCE, Card.SPADES);
-        System.out.println("Villain 1 Cards: " + villainCards[0][0].toString() + " & " + villainCards[0][1].toString());
 
         villainCards[1][0] = new Card(Card.TEN, Card.DIAMONDS);
         villainCards[1][1] = new Card(Card.JACK, Card.HEARTS);
+
+        fixedSimulation(heroCards, villainCards, 1000);
+    }
+
+    public static void fixedSimulation(Card[] heroCards, Card[][] villainCards, int numSimulations) {
+        CardDeck deck = new CardDeck();
+
+
+        System.out.println("Hero Cards: " + heroCards[0].toString() + " & " + heroCards[1].toString());
+
+
+        System.out.println("Villain 1 Cards: " + villainCards[0][0].toString() + " & " + villainCards[0][1].toString());
+
+
         System.out.println("Villain 2 Cards: " + villainCards[1][0].toString() + " & " + villainCards[1][1].toString());
 
         EquityCalculator calculator = new EquityCalculator(2, 100000 , heroCards, villainCards);
         calculator.runSimulation();
         calculator.printResults();
-
-
     }
 
 
-    public void randomSim() {
+    public void randomSim() {W
         CardDeck deck = new CardDeck();
         Card[] heroCards = new Card[2];
         Card[][] villainCards = new Card[2][2];
